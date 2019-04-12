6d1bf6f7b9aa5ae974fc8f23468a596e01c82dac
Michał Milanowski <mmilanow@intercars.eu>
2019-04-12 23:51:15 +0200

    first step of main page

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
+import { Row, Col, Button, Icon, Typography } from 'antd';
+
+const { Title } = Typography;
+
 class Main extends Component {
   render() {
-    return <div className="main-page" />;
+    return (
+      <Row type="flex" justify="center" align="middle" className="main-page">
+        <Col span={12}>
+          <Title>Pimp my mailbox!</Title>
+
+          <Button type="primary">
+            Forward
+            <Icon type="right" />
+          </Button>
+        </Col>
+      </Row>
+    );
   }
 }
 
