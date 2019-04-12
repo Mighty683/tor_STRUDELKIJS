86aa489331cbf420e4558aabb3b7a877e446bfc4
Tomasz Szarek <tomaszszarek@appjobs.com>
2019-04-13 00:33:07 +0200

    front-end/add google maps

--- frontend/src/components/map/index.jsx
+++ frontend/src/components/map/index.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 52,
+      lng: 22
+    },
+    zoom: 11
+  };
+
   render() {
-    return <div />;
+    return (
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.MAPS_KEY }}
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+      />
+    );
   }
 }
 
