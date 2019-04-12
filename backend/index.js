afdad8e2d7e496844068d26ea5903ecbf86f06fe
Tomasz Szarek <tomaszszarek@appjobs.com>
2019-04-12 22:20:39 +0200

    backend/init new api

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,28 @@
-function burnBabyBurn () {
+const csv = require('csvtojson')
+const express = require('express')
+const bodyParser = require('body-parser')
 
+function initServer () {
+  app.use(bodyParser.json())
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+  })
+  app.listen(1111, function () {
+    console.log('Listening 1111 port')
+  })
+}
+
+async function burnBabyBurn () {
+  try {
+    let mockData = await csv().fromFile('./mocks/pickup-points.csv')
+
+    if (mockData) {
+    }
+  } catch (e) {
+  }
+  
 }
 
 burnBabyBurn()
