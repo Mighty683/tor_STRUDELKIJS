2ddba6f6c126c55e6edd3b367887c84d8bb3f8ca
Tomasz Szarek <tomaszszarek@appjobs.com>
2019-04-12 22:03:40 +0200

    backend/cleanup-api

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,5 @@
-const Requests = require('./src/requests')
-const API = require('./src/api')
+function burnBabyBurn () {
 
-async function burnBabyBurn () {
-  try {
-    const  { data} = await Requests.getAccessToken()
-
-    if ( data && data.access_token) {
-      API.init(data.access_token)
-    }
-  } catch (e) {
-    console.log(e)
-  }
 }
 
 burnBabyBurn()
--- backend/src/config.js
+++
@@ -1,7 +0,0 @@
-// Sorry for hardcode
-const config = {
-  clientId: '319f18a113f8442e83cf31872a474e8f',
-  clientSecret: 'shTweha3mPqoKrYm6iyQQ6CTETHR3wuKi3v5M6nuU5SUtVjG5LeX1TTQ5uFd3qVs'
-}
-
-module.exports = config
