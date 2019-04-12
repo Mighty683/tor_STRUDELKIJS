cc8759375426e5fb599304acfca9010e280c0137
Marta <martaswierczewska@wp.pl>
2019-04-13 00:58:58 +0200

    rating added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Star from './components/star';
+import Rating from './components/rating';
+import data from './data.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Star data={'elo'} />
+        <Rating data={data[0]} />
         <header className="App-header">
           <p>
             Edit <code>src/App.js</code> and save to reload.
