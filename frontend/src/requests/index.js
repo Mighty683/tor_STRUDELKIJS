e37f8577ed64f9680bc03ae5c154e4b0e8212a3c
Tomasz Szarek <tomaszszarek@appjobs.com>
2019-04-13 09:02:53 +0200

    add flexible api endpoint

--- frontend/src/requests/index.js
+++ frontend/src/requests/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const defaultURL = 'http://localhost:1111';
+const defaultURL =
+  process.env.REACT_APP_URL_API || `http://${document.location.hostname}:1111`;
 
 export default {
   getPoints(latitude, longitude) {
